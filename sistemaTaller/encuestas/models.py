27e18755e859126d2fb76aa3843b91799b45c4a8
Kevin Antipa <kevinantipashi@gmail.com>
2019-09-24 08:06:11 -0300

    feat/creacion de modelo

--- sistemaTaller/encuestas/models.py
+++ sistemaTaller/encuestas/models.py
@@ -5,6 +5,14 @@ class Persona (models.Model):
     apellido = models.CharField(max_length=30)
     edad = models.CharField(max_length=30)
 
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
 class Ubicacion(models.Model):
     nombre = models.CharField(max_length=255)
     is_inventariable  = models.BooleanField(default=True)
@@ -14,3 +22,11 @@ class Ubicacion(models.Model):
     updated = models.DateTimeField(auto_now=True)
     #Lugar fisico en donde se encuentra el clilindro
     #patio planta, proceso planta, proceso externo, taller, cliente
+
+
+class Cliente(models.Model):
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    limite_credido = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
