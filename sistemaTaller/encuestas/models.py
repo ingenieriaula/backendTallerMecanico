612997a630ee5b9ffd1faa1bc9541d34ae4727bd
Kevin Antipa <kevinantipashi@gmail.com>
2019-09-23 21:25:16 -0300

    feat/creacion de modelo con el migrate

--- sistemaTaller/encuestas/models.py
+++ sistemaTaller/encuestas/models.py
@@ -4,3 +4,13 @@ class Persona (models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     edad = models.CharField(max_length=30)
+
+class Ubicacion(models.Model):
+    nombre = models.CharField(max_length=255)
+    is_inventariable  = models.BooleanField(default=True)
+    is_planta = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    #Lugar fisico en donde se encuentra el clilindro
+    #patio planta, proceso planta, proceso externo, taller, cliente
